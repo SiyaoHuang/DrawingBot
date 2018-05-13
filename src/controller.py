72a4f980188a317ca46d934227d4a5d238bb95bb
Daniel Kim <dsk252@cornell.edu>
2018-05-13 17:55:04 -0400

    fixed err

--- src/controller.py
+++ src/controller.py
@@ -87,7 +87,7 @@ class Controller(object):
 
 			# Keep going forward until a boundary is hit
 			self.bot.forward(self.fspeed)
-			time.sleep(1)
+			time.sleep(0.4)
 			while inBoundary(points):
 				p1, p2, p3 = points
 				self.pmap.addCalibration(p1, p2, p3)
